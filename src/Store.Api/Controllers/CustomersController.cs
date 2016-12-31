3afea4ad2435eed28dc6ef1c399b09d51035e259
Blaine Arnau <blainearnau@gmail.com>
2016-12-30 18:18:22 -0800

    First Commit

---
+++ src/Store.Api/Controllers/CustomersController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Api.Controllers
+{
+    [Route("api/users")]
+    public class CustomersController : Controller
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetCustomer(int id)
+        {
+            var customer = new CustomerDto()
+            {
+                Id = id,
+                FirstName = "Blaine",
+                LastName = "Arnau",
+                Email = "blainearnau@gmail.com"
+            };
+
+            return Ok(customer);
+        }
+
+        [HttpGet()]
+        public IActionResult GetCustomers()
+        {
+            var customers = new List<CustomerDto>()
+            {
+                new CustomerDto()
+                {
+                    Id = 1,
+                    FirstName = "Blaine",
+                    LastName = "Arnau",
+                    Email = "blainearnau@gmail.com"
+                },
+                new CustomerDto()
+                {
+                    Id = 2,
+                    FirstName = "Patty",
+                    LastName = "Roots",
+                    Email = "pattyroots@gmail.com"
+                }
+            };
+
+            return Ok(customers);
+        }
+
+        [HttpPost()]
+        public IActionResult AddCustomer()
+        {
+            string result = "Add/post customer route executed";
+
+            return Ok(result);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult PatchCustomer(int id)
+        {
+            string result = "Patch customer route executed for customer id: " + id;
+
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutCustomer(int id)
+        {
+            string result = "Put customer route executed for customer id: " + id;
+
+            return Ok(result);
+        }
+    }
+}
--- src/Store.Api/Startup.cs
+++ src/Store.Api/Startup.cs
@@ -5,33 +5,112 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using NLog.Extensions.Logging;
+
+using Microsoft.Extensions.Configuration;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace Store.Api
 {
     public class Startup
     {
+        public static IConfigurationRoot Configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appSettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddMvc()
+                // adds output formatters for xml
+                .AddMvcOptions(o => o.OutputFormatters.Add(
+                    new XmlDataContractSerializerOutputFormatter()));
+            // Change serializer settings to null which does not camel case names of properties in corresponding class.
+            //.AddJsonOptions(o =>
+            //{
+            //    if (o.SerializerSettings.ContractResolver != null)
+            //    {
+            //        var castedResolver = o.SerializerSettings.ContractResolver
+            //            as DefaultContractResolver;
+            //        castedResolver.NamingStrategy = null;
+            //    }
+            //});
+
+//#if DEBUG
+//            services.AddTransient<IMailService, LocalMailService>();
+//#else
+//            services.AddTransient<IMailService, CloudMailService>();
+//#endif
+
+
+            // Adds CityInfoContext to use sql server / Gets from appSettings.json
+
+            //var connectionString = Startup.Configuration["connectionStrings:cityInfoDBConnectionString"];
+            //services.AddDbContext<CityInfoContext>(o => o.UseSqlServer(connectionString));
+            //services.AddScoped<ICityInfoRepository, CityInfoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory/*, CityInfoContext cityInfoContext*/)
         {
             loggerFactory.AddConsole();
+
+            loggerFactory.AddDebug();
+
+            //loggerFactory.AddProvider(new NLog.Extensions.Logging.NLogLoggerProvider());
+
+            loggerFactory.AddNLog();
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseExceptionHandler();
+            }
+
+            //cityInfoContext.EnsureSeedDataForContext();
+
+            app.UseStatusCodePages();
+
+            //AutoMapper.Mapper.Initialize(cfg =>
+            //{
+            //    cfg.CreateMap<Entities.City, Models.CityWithoutPointsOfInterestDto>();
+            //    cfg.CreateMap<Entities.City, Models.CityDto>();
+            //    cfg.CreateMap<Entities.PointOfInterest, Models.PointOfInterestDto>();
+            //    cfg.CreateMap<Models.PointOfInterestForCreationDto, Entities.PointOfInterest>();
+            //    cfg.CreateMap<Models.PointOfInterestForUpdateDto, Entities.PointOfInterest>();
+            //    cfg.CreateMap<Entities.PointOfInterest, Models.PointOfInterestForUpdateDto>();
+            //});
+
+            app.UseMvc();
+            
+
+            //app.Run((context) =>
+            //{
+            //    throw new Exception("Example Exception.");
+            //});
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
+
